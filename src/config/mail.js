9dc7c868d81dab9c79a1253227d5b64c0408dded
Gabriel Batista <svbgabriel@gmail.com>
2019-08-11 12:21:33 -0300

    Implementa a notificação de cancelamentos

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '854120f5ec900b',
+    pass: '3d7dcd6647d1b6',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
