0a21ec2d38749339a0960280eb886f9060d84b43
Gabriel Batista <svbgabriel@proton.me>
2026-05-26 21:59:52 -0300

    feat: remove sentry

--- src/app.js
+++ src/app.js
@@ -4,10 +4,8 @@ import express from 'express';
 import cors from 'cors';
 import path from 'path';
 import Youch from 'youch';
-import * as Sentry from '@sentry/node';
 import 'express-async-errors';
 import routes from './routes';
-import sentryConfig from './config/sentry';
 
 import './database';
 
@@ -15,15 +13,12 @@ class App {
   constructor() {
     this.server = express();
 
-    Sentry.init(sentryConfig);
-
     this.middlewares();
     this.routes();
     this.exceptionHandler();
   }
 
   middlewares() {
-    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(cors());
     this.server.use(express.json());
     this.server.use(
@@ -34,7 +29,6 @@ class App {
 
   routes() {
     this.server.use(routes);
-    this.server.use(Sentry.Handlers.errorHandler());
   }
 
   exceptionHandler() {
